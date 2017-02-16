8757c3463bace994b9611752b288656ab81114a1
Lee Anne Mercado <lamercad@ucsd.edu>
2017-02-16 00:18:08 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,8 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
+
 }
 
 /*
@@ -26,7 +28,43 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
+
 	console.log("User clicked on project " + idNumber);
+
+	console.log("URL is " + URL);
+
+
+
+//	$(".details").html(projectDetails);
+
+}
+
+
+function addProject(result) {
+
+
+	console.log(result);
+
+	var projectHTML = '<a href="#" class="thumbnail">' +
+	'<img src=" ' + result['image'] + ' " class="img">' + 
+	'<p>' + result['title'] + '</p>' +
+	'<p><small>' + result['date'] + '</small></p></a>';
+
+	$("#project-container").html(projectHTML);
+	$("#project-description").html(result['summary']);
+
+
+
+	var projectDetails = '<class="thumbnail">' +
+	'<img src=" ' + result['image'] + ' " class="detailsImage">' + 
+	'<p>' + result['title'] + '</p>' +
+	'<p><small>' + result['date'] + '</small></p>' + 
+	'<p>' + result['summary'] + '</p>';
+	
+	$("#project" + result['id'] + " .details").html(projectDetails);
+
+
 }
 
 /*
@@ -35,4 +73,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", getColors);
+
+}
+
+function getColors(result){
+
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
